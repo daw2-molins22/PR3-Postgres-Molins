dbcaab6a3f19089a9cdbb1252d3221298fa10822
daw2-molins22 <79204486+daw2-molins22@users.noreply.github.com>
2022-01-10 14:52:01 +0100

    Add files via upload

---
+++ class.Individual.php
@@ -0,0 +1,26 @@
+<?php
+require_once('class.Entity.php');
+ 
+  class Individual extends Entity {
+   
+    public function __construct($userID) {
+      parent::__construct($userID);
+     
+      $this->propertyTable['firstname'] = 'name1';
+      $this->propertyTable['lastname'] = 'name2';
+
+    }
+
+    public function __toString() {
+      return $this->firstname . ' ' . $this->lastname;
+    }
+
+    public function validate() {
+      parent::validate();
+
+      //add individual-specific validation
+     
+    }
+ 
+  }
+?>
---
+++ class.Organization.php
@@ -0,0 +1,23 @@
+<?php
+require_once('class.Entity.php');
+
+  class Organization extends Entity {
+   
+    public function __construct($userID) {
+      parent::__construct($userID);
+     
+      $this->propertyTable['name'] = 'name1';
+
+    }
+
+    public function __toString() {
+      return $this->name;
+    }
+   
+    public function validate() {
+      parent::validate();
+      //do organization-specific validation
+    }
+ 
+  }
+?>
